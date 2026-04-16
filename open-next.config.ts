c05e14638e3c6cbb121a8ccb6e9c4c1b4a335753
zws13579 <63572410+zws13579@users.noreply.github.com>
2026-04-16 22:54:48 +0800

    gem1.3

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,3 +1,18 @@
 export default {
-  default: {},
+  default: {
+    override: {
+      wrapper: "cloudflare-node",
+      converter: "edge",
+      proxyExternalRequest: "fetch",
+    },
+  },
+  edgeExternals: ["node:crypto"],
+  middleware: {
+    external: true,
+    override: {
+      wrapper: "cloudflare-edge",
+      converter: "edge",
+      proxyExternalRequest: "fetch",
+    },
+  },
 };
